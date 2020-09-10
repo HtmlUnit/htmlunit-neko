f4f7c74177ca58391efeadbfe4c6c25eb2dc69cb
Ronald Brill <rbri@rbri.de>
2020-09-10 12:38:49 +0200

    implement better control for new line tests

--- src/test/java/net/sourceforge/htmlunit/cyberneko/FragmentContextStackTest.java
+++ src/test/java/net/sourceforge/htmlunit/cyberneko/FragmentContextStackTest.java
@@ -1,23 +1,17 @@
 package net.sourceforge.htmlunit.cyberneko;
 
-import java.io.StringReader;
-import java.io.StringWriter;
-
-import org.apache.xerces.xni.QName;
-import org.apache.xerces.xni.parser.XMLDocumentFilter;
-import org.apache.xerces.xni.parser.XMLInputSource;
-
 import junit.framework.TestCase;
-import net.sourceforge.htmlunit.cyberneko.parsers.DOMParser;
 
 /**
  * Unit tests for {@link HTMLTagBalancer}'s property {@link HTMLTagBalancer#FRAGMENT_CONTEXT_STACK}.
  *
  * @author Marc Guillemot
+ * @author Ronald Brill
  */
 public class FragmentContextStackTest extends TestCase {
 
     private static final String NL = System.lineSeparator();
+    private static final String[] FEATURES = {"http://cyberneko.org/html/features/balance-tags/document-fragment"};
 
     public void testSimple() throws Exception {
         final String expected = "(div" + NL
@@ -25,12 +19,12 @@ public class FragmentContextStackTest extends TestCase {
             + "\"hello" + NL
             + ")span" + NL
             + ")div" + NL;
-        doTest("<div><span>hello</span>", new String[] { "html", "body" }, expected);
+        GeneralTest.doTest("<div><span>hello</span>", new String[] { "html", "body" }, expected, FEATURES);
 
-        doTest("<div><span>hello</span>", new String[] { "html" }, expected);
+        GeneralTest.doTest("<div><span>hello</span>", new String[] { "html" }, expected, FEATURES);
 
-        doTest("<div><span>hello</span>", new String[]{}, expected);
-        doTest("<div><span>hello</span>", null, expected);
+        GeneralTest.doTest("<div><span>hello</span>", new String[]{}, expected, FEATURES);
+        GeneralTest.doTest("<div><span>hello</span>", null, expected, FEATURES);
     }
 
     public void testTR() throws Exception {
@@ -39,57 +33,30 @@ public class FragmentContextStackTest extends TestCase {
             + "\"hello" + NL
             + ")td" + NL
             + ")tr" + NL;
-        doTest("<tr><td>hello</td></tr>", new String[] { "html", "body", "table", "tbody" }, expected);
+        GeneralTest.doTest("<tr><td>hello</td></tr>", new String[] { "html", "body", "table", "tbody" }, expected, FEATURES);
         expected = "(TBODY" + NL
             + expected
             + ")TBODY\n";
-        doTest("<tr><td>hello</td></tr>", new String[] { "html", "body", "table" }, expected);
-        doTest("<tr><td>hello</td></tr>", new String[] { "html", "body" }, "\"hello");
+        GeneralTest.doTest("<tr><td>hello</td></tr>", new String[] { "html", "body", "table" }, expected, FEATURES);
+        GeneralTest.doTest("<tr><td>hello</td></tr>", new String[] { "html", "body" }, "\"hello", FEATURES);
     }
 
     public void testFragmentShouldNotCloseContextStack() throws Exception {
         final String expected = "\"helloworld\n";
-        doTest("hello</div>world", new String[] { "html", "body", "div" }, expected);
-        doTest("hello</span>world", new String[] { "html", "body", "div", "span" }, expected);
+        GeneralTest.doTest("hello</div>world", new String[] { "html", "body", "div" }, expected, FEATURES);
+        GeneralTest.doTest("hello</span>world", new String[] { "html", "body", "div", "span" }, expected, FEATURES);
     }
 
     public void testFragmentShouldNotCloseContextStackPHack() throws Exception {
-        doTest("<p>hello world</p>", new String[] { "html", "body", "p" }, ")null" + NL + "(p" + NL + "\"hello world" + NL + ")p");
-        doTest("<p>hello world", new String[] { "html", "body", "p" }, ")null" + NL + "(p" + NL + "\"hello world" + NL + ")p");
+        GeneralTest.doTest("<p>hello world</p>", new String[] { "html", "body", "p" }, ")null" + NL + "(p" + NL + "\"hello world" + NL + ")p", FEATURES);
+        GeneralTest.doTest("<p>hello world", new String[] { "html", "body", "p" }, ")null" + NL + "(p" + NL + "\"hello world" + NL + ")p", FEATURES);
 
-        doTest("<p>hello world</p>", new String[] { "html", "body", "p", "span"}, ")null" + NL + ")null" + NL + "(p" + NL + "\"hello world" + NL + ")p");
+        GeneralTest.doTest("<p>hello world</p>", new String[] { "html", "body", "p", "span"}, ")null" + NL + ")null" + NL + "(p" + NL + "\"hello world" + NL + ")p", FEATURES);
     }
 
     public void testFragmentShouldNotCloseSelect() throws Exception {
-        doTest("<select><option>Two</option></select>", new String[] { "html", "body", "select" },
-                ")null" + NL + "(select" + NL + "(option" + NL + "\"Two" + NL + ")option" + NL + ")select");
-    }
-
-    private static void doTest(final String html, final String[] contextStack,
-            final String expected) throws Exception {
-        final DOMParser parser = new DOMParser();
-        parser.setFeature("http://cyberneko.org/html/features/balance-tags/document-fragment", true);
-        if (contextStack != null) {
-            parser.setProperty("http://cyberneko.org/html/properties/balance-tags/fragment-context-stack", toQNames(contextStack));
-        }
-
-        final StringWriter out = new StringWriter();
-        final XMLDocumentFilter[] filters = { new Writer(out) };
-        parser.setProperty("http://cyberneko.org/html/properties/filters", filters);
-
-        final StringReader sr = new StringReader(html);
-        final XMLInputSource in = new XMLInputSource(null, "foo", null, sr, null);
-        parser.parse(in);
-
-        assertEquals(expected.trim(), out.toString().trim());
-    }
-
-    private static QName[] toQNames(final String[] tags) {
-        final QName[] qnames = new QName[tags.length];
-        for (int i = 0; i < tags.length; ++i) {
-            qnames[i] = new QName(null, tags[i], null, null);
-        }
-
-        return qnames;
+        GeneralTest.doTest("<select><option>Two</option></select>", new String[] { "html", "body", "select" },
+                ")null" + NL + "(select" + NL + "(option" + NL + "\"Two" + NL + ")option" + NL + ")select",
+                FEATURES);
     }
 }
