a2205eedb5293f6958f32c549f3b5161c01519da
Ronald Brill <rbri@rbri.de>
2023-03-18 11:54:44 +0100

    update copyright

--- src/test/java/org/htmlunit/cyberneko/GeneralTest.java
+++ src/test/java/org/htmlunit/cyberneko/GeneralTest.java
@@ -1,8 +1,20 @@
-/**
- * Unit tests for the parser not done as {@link CanonicalTest}.
+/*
+ * Copyright 2002-2009 Andy Clark, Marc Guillemot
+ * Copyright 2017-2023 Ronald Brill
  *
- * @author Ronald Brill
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 package org.htmlunit.cyberneko;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,6 +30,8 @@ import org.htmlunit.cyberneko.xerces.xni.parser.XMLInputSource;
 import org.junit.jupiter.api.Test;
 
 /**
+ * Unit tests for the parser not done as {@link CanonicalTest}.
+ *
  * @author Ronald Brill
  */
 public class GeneralTest {
---
+++ src/test/java/org/htmlunit/cyberneko/ReadmeTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2002-2009 Andy Clark, Marc Guillemot
+ * Copyright 2017-2023 Ronald Brill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.htmlunit.cyberneko;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.htmlunit.cyberneko.html.dom.HTMLDocumentImpl;
+import org.htmlunit.cyberneko.parsers.DOMParser;
+import org.htmlunit.cyberneko.parsers.SAXParser;
+import org.htmlunit.cyberneko.xerces.xni.parser.XMLInputSource;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.NodeList;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+
+/**
+ * Unit tests for the code from the readme.
+ *
+ * @author Ronald Brill
+ */
+public class ReadmeTest {
+
+    @Test
+    public void domParser() throws Exception {
+        final String html =
+                " <!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<body>\n"
+                + "<h1>NekoHtml</h1>\n"
+                + "</body>\n"
+                + "</html>";
+
+        final StringReader sr = new StringReader(html);
+        final XMLInputSource in = new XMLInputSource(null, "foo", null, sr, null);
+
+        // use the provided simple DocumentImpl
+        final DOMParser parser = new DOMParser(HTMLDocumentImpl.class);
+        parser.parse(in);
+
+        HTMLDocumentImpl doc = (HTMLDocumentImpl) parser.getDocument();
+        NodeList headings = doc.getElementsByTagName("h1");
+
+        System.out.println(headings.item(0));
+    }
+
+    @Test
+    public void test() throws IOException {
+        final String html =
+                " <!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<body>\n"
+                + "<h1>NekoHtml</h1>\n"
+                + "</body>\n"
+                + "</html>";
+
+        final StringReader sr = new StringReader(html);
+        final XMLInputSource in = new XMLInputSource(null, "foo", null, sr, null);
+
+        final SAXParser parser = new SAXParser();
+
+        ContentHandler myContentHandler = new MyContentHandler();
+        parser.setContentHandler(myContentHandler);
+
+        parser.parse(in);
+    }
+
+
+    private static class MyContentHandler implements ContentHandler {
+        @Override
+        public void startPrefixMapping(String prefix, String uri) throws SAXException {
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+        }
+
+        @Override
+        public void startDocument() throws SAXException {
+        }
+
+        @Override
+        public void skippedEntity(String name) throws SAXException {
+        }
+
+        @Override
+        public void setDocumentLocator(Locator locator) {
+        }
+
+        @Override
+        public void processingInstruction(String target, String data)
+                throws SAXException {
+        }
+
+        @Override
+        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
+        }
+
+        @Override
+        public void endPrefixMapping(String prefix) throws SAXException {
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+        }
+
+        @Override
+        public void endDocument() throws SAXException {
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+        }
+    }
+}
