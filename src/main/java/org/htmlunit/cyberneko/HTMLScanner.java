cfae62eee7177d750b71782ba08acb4cb2c04460
Ronald Brill <rbri@rbri.de>
2025-11-22 14:58:56 +0100

    typo

--- src/main/java/org/htmlunit/cyberneko/HTMLScanner.java
+++ src/main/java/org/htmlunit/cyberneko/HTMLScanner.java
@@ -502,7 +502,7 @@ public class HTMLScanner implements XMLDocumentSource, XMLLocator, HTMLComponent
     protected final XMLString fStringBuffer = new XMLString();
 
     /** String buffer used when resolving entity refs. */
-    final XMLString fStringBufferEntiyRef = new XMLString();
+    final XMLString fStringBufferEntityRef = new XMLString();
     final XMLString fStringBufferPlainAttribValue = new XMLString();
 
     final XMLString fScanUntilEndTag = new XMLString();
@@ -3280,8 +3280,8 @@ public class HTMLScanner implements XMLDocumentSource, XMLLocator, HTMLComponent
                 }
 
                 if (c == '&') {
-                    scanEntityRef(fStringBufferEntiyRef, plainAttribValue, false);
-                    attribValue.append(fStringBufferEntiyRef);
+                    scanEntityRef(fStringBufferEntityRef, plainAttribValue, false);
+                    attribValue.append(fStringBufferEntityRef);
                 }
                 else {
                     if (!attribValue.appendCodePoint(c)) {
@@ -3355,7 +3355,7 @@ public class HTMLScanner implements XMLDocumentSource, XMLLocator, HTMLComponent
                 }
                 else if (c == '&') {
                     isStart = false;
-                    final int ce = scanEntityRef(fStringBufferEntiyRef, plainAttribValue, false);
+                    final int ce = scanEntityRef(fStringBufferEntityRef, plainAttribValue, false);
                     if (ce != -1) {
                         if (!attribValue.appendCodePoint(ce)) {
                             if (fReportErrors_) {
@@ -3364,7 +3364,7 @@ public class HTMLScanner implements XMLDocumentSource, XMLLocator, HTMLComponent
                         }
                     }
                     else {
-                        attribValue.append(fStringBufferEntiyRef);
+                        attribValue.append(fStringBufferEntityRef);
                     }
                     prevSpace = false;
                 }
